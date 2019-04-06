e3ecd34f5f97907603c1db77fc75ff37726458b2
zulfumehmet <zulfumehmet@gmail.com>
2019-04-06 17:07:21 +0300

    Update header.php

--- V1.0/header.php
+++ V1.0/header.php
@@ -5,8 +5,9 @@ include ("inc/db.php");
 <html lang="TR">
 <head>
 	<title>Güncel Döviz Kurları | Zulfu Mehmet</title>
+	<base href="https://www.zulfumehmet.tk">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8">
 	<!-- Font -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
